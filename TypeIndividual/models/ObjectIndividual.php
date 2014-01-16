164d5e820a53b41105838812454116fa41298433
Jacob Morrison <jacob@infinitecascade.com>
2014-01-15 18:24:19 -0800

    sub descriptor for individual

--- TypeIndividual/models/ObjectIndividual.php
+++ TypeIndividual/models/ObjectIndividual.php
@@ -34,7 +34,7 @@ use cascade\models\Registry;
 class ObjectIndividual extends \cascade\components\types\ActiveRecord
 {
 	public $descriptorField = ['first_name', 'middle_name', 'last_name'];
-
+	public $subdescriptorFields = ['parent:Account'];
 	/**
 	 * @inheritdoc
 	 */
