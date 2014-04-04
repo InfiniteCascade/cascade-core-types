ca93c98687f5b9de28e6c71221bcca3b0c0ddaca
Jacob Morrison <jmorrison@psesd.org>
2014-04-03 18:17:12 -0700

    minor fix

--- TypeFile/models/ObjectFile.php
+++ TypeFile/models/ObjectFile.php
@@ -56,9 +56,9 @@ class ObjectFile extends \cascade\components\types\ActiveRecord
 		}
 	}
 
-	public static function createQuery($config = [])
+	public static function createQuery()
 	{
-		$query = parent::createQuery($config);
+		$query = parent::createQuery();
 		$alias = $query->primaryAlias;
 		$query->select(['`'. $alias .'`.*', '`storage`.`file_name` as `labelName`']);
 		$query->join('INNER JOIN', Storage::tableName() .' storage', '`storage`.`id` = `'.$alias.'`.`storage_id`');
